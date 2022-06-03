ce7e69bc960e635deef0dac19e6f0fbfdefe8559
raphtalia <59540801+raphtalia@users.noreply.github.com>
2022-06-03 09:24:04 -0500

    Re-implemented custom rounding function

--- src/init.client.lua
+++ src/init.client.lua
@@ -3,11 +3,15 @@ local sha1 = require(script.sha1)
 local HttpService = game:GetService("HttpService")
 local UserInputService = game:GetService("UserInputService")
 
+local function round(x, y)
+    return math.round(x / y) * y
+end
+
 print(sha1.sha1(HttpService:JSONEncode({
     -- Any number of data points can be added here
     time = {
         -- CPU start is the main data point used
-        cpuStart = math.round(tick() - os.clock()),
+        cpuStart = round(tick() - os.clock(), 5),
         timezone = os.date("%Z"),
         isDST = os.date("*t").isdst,
     },
